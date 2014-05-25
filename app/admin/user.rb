526bb8a3f33dcb410da6d7f369b99d5485d20fb9
Victor Borja <vborja@apache.org>
2014-05-25 11:41:46 -0500

    show user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -9,4 +9,16 @@ ActiveAdmin.register User do
     end
     actions
   end
+
+  show do
+    attributes_table do
+      row :name
+      row :identities do |user|
+        user.identities.map do |identity|
+          link_to(identity.provider, identity.url, :target => "_blank")
+        end.join(' ').html_safe
+      end
+    end
+    active_admin_comments
+  end
 end
