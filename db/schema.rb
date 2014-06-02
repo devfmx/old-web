1a576bbc67133bb7fc9acd3b477eed786ad6d1ec
Victor Borja <vborja@apache.org>
2014-06-02 18:45:22 -0500

    Twitter doesnt provide user email, Make user email optional.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601094414) do
+ActiveRecord::Schema.define(version: 20140602225507) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20140601094414) do
   add_index "identities", ["user_id"], name: "identities_user_id_fk", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
