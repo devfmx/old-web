912a763afeb4e1904b30b87f40c25d86e282d6c0
Victor Borja <vborja@apache.org>
2014-06-09 21:22:52 -0500

    Specify asset host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = Settings.smtp_settings
   config.action_mailer.default_url_options = {:host => Settings.domain}
+
+  config.action_controller.asset_host = "http://" + Settings.domain
   
 end
